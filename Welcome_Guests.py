27ec4d4b8e6ecc41e946216418b6f12770c4924a
yousufafroze <44031706+yousufafroze@users.noreply.github.com>
2020-05-13 19:39:37 -0400

    Update Welcome_Guests.py

--- Welcome_Guests.py
+++ Welcome_Guests.py
@@ -5,7 +5,7 @@ import time
 import pyttsx3
 import os
 
-
+## Used the demo for OpenCV
 
 # Download the helper library from https://www.twilio.com/docs/python/install
 from twilio.rest import Client
@@ -23,24 +23,16 @@ def sms(statement):
 
 
 
-
-
-
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
 #   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
 #   2. Only detect faces in every other frame of video.
 
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
 
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("IMG_0253.JPG")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+my_image = face_recognition.load_image_file("IMG_0253.JPG")
+my_face_encoding = face_recognition.face_encodings(my_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
 # biden_image = face_recognition.load_image_file("IMG_0386.jpg")
@@ -48,7 +40,7 @@ obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
-    obama_face_encoding
+    my_face_encoding
 ]
 known_face_names = [
     "Yousuf"
